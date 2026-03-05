2dfe38545d30e2e82ab9e566c5674d882e2823d2
hemanth3002t <hemanthvenkat899@gmail.com>
2026-03-05 18:37:58 +0530

    Updated app for Railway and Render deployment

--- app.py
+++ app.py
@@ -2,33 +2,35 @@ from flask import Flask, render_template, redirect, flash
 import mysql.connector
 from dotenv import load_dotenv
 import os
+
 load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = "secretkey123"
 
-
 db = mysql.connector.connect(
     host=os.getenv("DB_HOST"),
+    port=os.getenv("DB_PORT"),
     user=os.getenv("DB_USER"),
     password=os.getenv("DB_PASSWORD"),
     database=os.getenv("DB_NAME")
 )
 
 cursor = db.cursor()
-
 @app.route('/')
 def index():
     cursor.execute("SELECT * FROM slots")
     slots = cursor.fetchall()
     return render_template("index.html", slots=slots)
+
+
 @app.route('/book/<int:id>')
 def book(id):
 
     cursor.execute("SELECT status FROM slots WHERE id=%s", (id,))
     status = cursor.fetchone()
 
-    if status[0] == "Available":
+    if status and status[0] == "Available":
         cursor.execute("UPDATE slots SET status='Booked' WHERE id=%s", (id,))
         db.commit()
         flash("Slot booked successfully!")
@@ -36,8 +38,6 @@ def book(id):
         flash("Slot already booked.")
 
     return redirect('/')
-
-
 
 @app.route('/cancel/<int:id>')
 def cancel(id):
@@ -59,4 +59,5 @@ def booked():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
